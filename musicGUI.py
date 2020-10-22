cbae7171402586fef9bdc8af7bc502e02ef234b4
Tazmondo <fondcoolman@outlook.com>
2020-10-22 21:11:04 +0100

    deleted old gui py

--- musicGUI.py
+++ musicGUI.py
@@ -1,48 +1,14 @@
 import tkinter as tk
 from tkinter import ttk
 
-class tut1:
-    def __init__(self):
-        root = tk.Tk()
-        root.title("Feet to Meters")
-
-        mainframe = ttk.Frame(root, padding="3 3 12 12")
-        mainframe.grid(column=0, row=0, sticky=(tk.N, tk.W, tk.E, tk.S))
-        root.columnconfigure(0, weight=1)
-        root.rowconfigure(0, weight=1)
-
-        self.feet = tk.StringVar()
-        feet_entry = ttk.Entry(mainframe, width=7, textvariable=self.feet)
-        feet_entry.grid(column=2, row=1, sticky=(tk.W, tk.E))
-
-        self.meters = tk.StringVar()
-        ttk.Label(mainframe, textvariable=self.meters).grid(column=2, row=2, sticky=(tk.W, tk.E))
-
-        ttk.Button(mainframe, text="Calculate", command=self.calculate).grid(column=3, row=3, sticky=tk.W)
-
-        ttk.Label(mainframe, text="feet").grid(column=3, row=1, sticky=tk.W)
-        ttk.Label(mainframe, text="is equivalent to").grid(column=1, row=2, sticky=tk.E)
-        ttk.Label(mainframe, text="meters").grid(column=3, row=2, sticky=tk.W)
-
-        for child in mainframe.winfo_children():
-            child.grid_configure(padx=5, pady=5)
-
-        feet_entry.focus()
-        root.bind("<Return>", self.calculate)
-
-        root.mainloop()
-    def calculate(self,*args):
-        try:
-            value = float(self.feet.get())
-            self.meters.set(int(0.3048 * value * 10000.0 + 0.5)/10000.0)
-        except ValueError:
-            pass
 
 class musicgui:
     playlists = None
     selectedplaylist = None
 
     songqueue = None
+
+    output = ""
 
     def __init__(self, playlistnames):
         root = tk.Tk()
